c99ffcf56926610a9c3c7d53743020113040db69
Iqbal Akbari <achmadiqbalakbari@gmail.com>
2024-04-16 19:20:54 +0700

    feat: revisi regex

--- processor.py
+++ processor.py
@@ -1,8 +1,19 @@
 import re
 from numpy import random
 
+import argparse
 
-data = open('gcode/lorem.gcode', 'r').read()
+parser = argparse.ArgumentParser(description="processortt",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-f", "--file", default='', type=str, help="specify file name")
+parser.add_argument("-o", "--output", default='', type=str, help="specify output file name")
+
+
+args = parser.parse_args()
+config = vars(args)
+print(config)
+
+data = open(config["file"], 'r').read()
 data = re.sub(r'(?<=([.]\d{3}))\d*','',data)
 data = re.sub(r'G0','G0 Z6\nG0',data)
 for i in range(1,5):
@@ -16,24 +27,27 @@ data = re.sub(r'^([^\n]*\n){2}','G21\nG90\nG0 Z5 F1000\n',data)
 data = re.sub(r'\nM2','G0 Z10',data)
 
 #offset
-data = re.sub(r'(?<=X)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+40),data)
-data = re.sub(r'(?<=Y)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+24),data)
+data = re.sub(r'(?<=X)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+42),data)
+data = re.sub(r'(?<=Y)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+17),data)
 data = re.sub(r'(?<=([.]\d{3}))\d*','',data)
 
+
+data = re.sub(r';.*',';svg',data)
 # random shift
 #print(indices)
-indices = [(m.start(0), m.end(0)) for m in re.finditer(r'(?<=svg#Layer_1 > g > path)\nG0 Z6\nG0 X\d*[.]\d* Y\d*[.]\d*\nG0 Z5\n', data)]
+indices = [(m.start(0), m.end(0)) for m in re.finditer(r'(?<=;svg)\nG0 Z6\nG0 X\d*[.]\d* Y\d*[.]\d*\nG0 Z5\n', data)]
 for id,n in enumerate(indices[:-1]):
     print(id)
     off = random.normal(0,0.3)
-    windices = [(m.start(0), m.end(0)) for m in re.finditer(r'(?<=svg#Layer_1 > g > path)\nG0 Z6\nG0 X\d*[.]\d* Y\d*[.]\d*\nG0 Z5\n', data)]
+    windices = [(m.start(0), m.end(0)) for m in re.finditer(r'(?<=;svg)\nG0 Z6\nG0 X\d*[.]\d* Y\d*[.]\d*\nG0 Z5\n', data)]
     st = windices[id][1]
     su = windices[id][0]
     en = windices[id+1][0]
-    data = data[:su]+re.sub(r'(?<=Y)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+off),data[su:st])+data[st:]
     data = data[:st]+re.sub(r'(?<=Y)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+off),data[st:en])+data[en:]
+    data = data[:su]+re.sub(r'(?<=Y)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+off),data[su:st])+data[st:]
+
 
 #data = re.sub(r'(?<=Y)(\d*[.]\d*|(\d*))',lambda m : str(float(m.group(0))+random.normal(0,0.1)),data)
 
-f = open("gcode/lorem3.gcode", "w")
+f = open(config["output"], "w")
 f.write(data) 
